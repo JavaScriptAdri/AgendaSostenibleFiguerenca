42e974f17c5879ce6d874cc0597f5bc69c79f075
Leo <lcampaprost@cendrassos.net>
2024-10-28 19:05:38 +0100

    implementacio emeset-lite

--- public/js/Index.js
+++ public/js/Index.js
@@ -11,3 +11,12 @@ function Ocult_registre() {
     document.getElementById('Registrar-ocult').style.display = 'none';
     document.getElementById('Iniciar-ocult').style.display = 'none';
 }
+function RegistreUsuaris() {
+    const usuariNR = document.querySelector('input[name="UsuariNR"]').value;
+    const contrasenyanR1 = document.querySelector('input[name="ContrasenyaNR1"]').value;
+    const contrasenyanR2 = document.querySelector('input[name="ContrasenyaNR2"]').value;
+}
+function LoginUsuaris() {
+    const usuariR = document.querySelector('input[name="UsuariR"]').value;
+    const contrasenyaR = document.querySelector('input[name="ContrasenyaR"]').value;
+}
