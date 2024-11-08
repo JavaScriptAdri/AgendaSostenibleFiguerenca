bfc8f8e238fa191e4402d523f3bde5085d95cc0e
Leo <lcampaprost@cendrassos.net>
2024-11-08 16:34:24 +0100

    solucionar vistes

--- src/views/index.php
+++ src/views/index.php
@@ -53,12 +53,13 @@ include 'php/cookiePerDefecte.php'; // Inclou el fitxer que crea la cookie per d
                 </div>
             </div>
         </div>
+
         <div class="top_llegenda">
             <img src="imatges/IconaP2.png" alt="Agenda Sostenible Figuerenca" title="Agenda Sostenible Figuerenca">
             <a href="index.php"><button title="Home" class="Home">Home</button></a>
-            <a href="/src/views/anuncis.php"><button title="Anuncis" class="Anuncis">Anuncis</button></a>
-            <a href="/src/views/consells.php"><button title="Consells" class="Consells">Consells</button></a>  
-            <a href="/src/views/esdeveniments.php"><button title="Buscador Esdeveniments" class="EsdevenimentsBuscador">Buscador esdeveniments</button></a>          
+            <a href="index.php?r=anuncis"><button title="Anuncis" class="Anuncis">Anuncis</button></a>
+            <a href="index.php?r=consells"><button title="Consells" class="Consells">Consells</button></a> 
+            <a href="index.php?r=esdeveniments"><button title="Buscador Esdeveniments" class="EsdevenimentsBuscador">Buscador esdeveniments</button></a>          
        </div>
 
         <div class="Titul_principal">
